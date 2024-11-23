07957e85121a3dfa480babcb465e04c9ccab6f86
forbesman12 <forbesman12@gmail.com>
2024-11-23 11:55:59 +0100

    updated 23

--- script.js
+++ script.js
@@ -167,3 +167,8 @@ function goToShop(filter) {
     // Redirect to the shop page with the filter as a query parameter
     window.location.href = `shop.html?filter=${encodeURIComponent(filter)}`;
 }
+(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1203249');
